cb11e4f34b7f7d4d6664a029ff5e408db31a5f4a
Zsj <jshazsj@qq.com>
2020-12-15 13:39:24 +0800

    initial commit

---
+++ FlyAudio.cpp
@@ -0,0 +1,114 @@
+﻿// FlyAudio.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "FlyAudio.h"
+#include "bass.h"
+#include <vector>
+#include <iostream>
+#pragma comment (lib, "bass.lib")
+
+extern "C" {
+	int deviceCount = 0;
+	HSTREAM stream = 0;
+
+	FLYAUDIO_API void init(int device, int freq) {
+		// BASS_Init(-1, 44100, 0, 0, 0);
+		BASS_Init(device, freq, 0, 0, 0);
+	}
+
+	FLYAUDIO_API DEVICE_INFO** getDevices() {
+		int a;
+		deviceCount = 0;
+		std::vector<DEVICE_INFO*> vec;
+		BASS_DEVICEINFO info;
+		for (a = 1; BASS_GetDeviceInfo(a, &info); a++) {
+			if (info.flags & BASS_DEVICE_ENABLED) {
+				deviceCount++;
+				DEVICE_INFO *dinfo = new DEVICE_INFO();
+				dinfo->driver = info.driver;
+				dinfo->name = info.name;
+				dinfo->flags = info.flags;
+				dinfo->isDefault = (info.flags & BASS_DEVICE_DEFAULT) == 0 ? 0 : 1;
+				vec.push_back(dinfo);
+			}
+		}
+
+		DEVICE_INFO** ret = new DEVICE_INFO*[deviceCount];
+		for (int i = 0; i < deviceCount; i++) {
+			ret[i] = vec[i];
+		}
+		return ret;
+	}
+
+	FLYAUDIO_API int getDeviceCount() {
+		return deviceCount;
+	}
+
+	FLYAUDIO_API bool setDevice(int id) {
+		return BASS_SetDevice(id);
+	}
+
+	FLYAUDIO_API int loadFile(const char* file) {
+		stream = BASS_StreamCreateFile(FALSE, file, 0, 0, 0);
+		if (stream != 0) {
+			return 0;
+		} else {
+			return BASS_ErrorGetCode();
+		}
+	}
+
+	FLYAUDIO_API bool play() {
+		return BASS_ChannelPlay(stream, false);
+	}
+
+	FLYAUDIO_API bool pause() {
+		return BASS_ChannelPause(stream);
+	}
+
+	FLYAUDIO_API bool stop() {
+		return BASS_ChannelStop(stream);
+	}
+
+	FLYAUDIO_API double getDuration() {
+		QWORD len = BASS_ChannelGetLength(stream, BASS_POS_BYTE);
+		return BASS_ChannelBytes2Seconds(stream, len);
+	}
+
+	FLYAUDIO_API double getPosition() {
+		QWORD len = BASS_ChannelGetPosition(stream, BASS_POS_BYTE);
+		return BASS_ChannelBytes2Seconds(stream, len);
+	}
+
+	FLYAUDIO_API void setPosition(double sec) {
+		QWORD pos = BASS_ChannelSeconds2Bytes(stream, sec);
+		BASS_ChannelSetPosition(stream, pos, BASS_POS_BYTE);
+	}
+
+	FLYAUDIO_API int setVolume(int vol) {
+		float _vol = (float) (vol < 0 ? 0 : (vol > 100 ? 100 : vol)) / (float) 100.0;
+		BASS_ChannelSetAttribute(stream, BASS_ATTRIB_VOL, _vol);
+		return getVolume();
+	}
+
+	FLYAUDIO_API int getVolume() {
+		float vol;
+		BASS_ChannelGetAttribute(stream, BASS_ATTRIB_VOL, &vol);
+		return (int) (100.0 * vol);
+	}
+
+	FLYAUDIO_API float getCpu() {
+		return BASS_GetCPU();
+	}
+
+	FLYAUDIO_API void freeStream() {
+		BASS_StreamFree(stream);
+		stream = 0;
+	}
+
+	FLYAUDIO_API void close() {
+		BASS_StreamFree(stream);
+		BASS_Free();
+	}
+}
---
+++ FlyAudio.h
@@ -0,0 +1,50 @@
+﻿// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 FLYAUDIO_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// FLYAUDIO_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+
+
+#ifdef FLYAUDIO_EXPORTS
+#define FLYAUDIO_API __declspec(dllexport)
+#else
+#define FLYAUDIO_API __declspec(dllimport)
+#endif
+
+struct DEVICE_INFO {
+#if defined(_WIN32_WCE) || (WINAPI_FAMILY && WINAPI_FAMILY!=WINAPI_FAMILY_DESKTOP_APP)
+	const wchar_t* name;	// description
+	const wchar_t* driver;	// driver
+#else
+	const char* name;	// description
+	const char* driver;	// driver
+#endif
+	DWORD flags;
+	int isDefault;
+};
+
+extern "C" {
+	FLYAUDIO_API void init(int, int);
+	FLYAUDIO_API DEVICE_INFO** getDevices();
+	FLYAUDIO_API int getDeviceCount();
+	FLYAUDIO_API bool setDevice(int);
+	FLYAUDIO_API int loadFile(const char*);
+
+	FLYAUDIO_API bool play();
+	FLYAUDIO_API bool pause();
+	FLYAUDIO_API bool stop();
+
+	FLYAUDIO_API double getDuration();
+
+	FLYAUDIO_API double getPosition();
+	FLYAUDIO_API void setPosition(double);
+
+	FLYAUDIO_API int setVolume(int);
+	FLYAUDIO_API int getVolume();
+
+	FLYAUDIO_API float getCpu();
+
+	FLYAUDIO_API void freeStream();
+	FLYAUDIO_API void close();
+}
