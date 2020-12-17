0c34fb1045a5578afab6f1c775f0aa1e94800256
Zsj <jshazsj@qq.com>
2020-12-17 18:06:00 +0800

    Merge complete

---
+++ FlyAudio.cpp
@@ -0,0 +1,170 @@
+﻿// FlyAudio.cpp : 定义 DLL 的导出函数。
+//
+
+#pragma once
+#ifdef _WIN32
+#include "pch.h"
+#include "framework.h"
+#endif
+
+#include "FlyAudio.h"
+#include "bass.h"
+#include <taglib/tag.h>
+#include <taglib/fileref.h>
+#include <taglib/tpropertymap.h>
+#include <taglib/tstring.h>
+#include <vector>
+#include <iostream>
+
+#ifdef _WIN32
+#pragma comment (lib, "bass.lib")
+#pragma comment (lib, "tag.lib")
+#pragma comment (lib, "tag_c.lib")
+#else
+#endif
+
+extern "C" {
+	int deviceCount = 0;
+	HSTREAM stream = 0;
+
+	FLYAUDIO_API void init(int device, int freq) {
+		// BASS_Init(-1, 44100, 0, 0, 0);
+		BASS_Init(device, freq, 0, 0, 0);
+	}
+
+	FLYAUDIO_API DEVICE_INFO** getDevices() {
+		int a;
+		deviceCount = 0;
+		std::vector<DEVICE_INFO*> vec;
+		BASS_DEVICEINFO info;
+		BASS_SetConfig(BASS_CONFIG_DEV_DEFAULT, true);
+		for (a = 1; BASS_GetDeviceInfo(a, &info); a++) {
+			if (info.flags & BASS_DEVICE_ENABLED) {
+				deviceCount++;
+				DEVICE_INFO *dinfo = new DEVICE_INFO();
+				dinfo->driver = info.driver;
+				dinfo->name = info.name;
+				dinfo->flags = info.flags;
+				dinfo->isDefault = (info.flags & BASS_DEVICE_DEFAULT) == 0 ? 0 : 1;
+				vec.push_back(dinfo);
+			}
+		}
+
+		DEVICE_INFO** ret = new DEVICE_INFO*[deviceCount];
+		for (int i = 0; i < deviceCount; i++) {
+			ret[i] = vec[i];
+		}
+		return ret;
+	}
+
+	FLYAUDIO_API int getDeviceCount() {
+		return deviceCount;
+	}
+
+	FLYAUDIO_API bool setDevice(int id) {
+		return BASS_SetDevice(id);
+	}
+
+	FLYAUDIO_API int loadFile(const char* file) {
+		stream = BASS_StreamCreateFile(FALSE, file, 0, 0, 0);
+		if (stream != 0) {
+			return 0;
+		} else {
+			return BASS_ErrorGetCode();
+		}
+	}
+
+	FLYAUDIO_API bool play() {
+		return BASS_ChannelPlay(stream, false);
+	}
+
+	FLYAUDIO_API bool pause() {
+		return BASS_ChannelPause(stream);
+	}
+
+	FLYAUDIO_API bool stop() {
+		return BASS_ChannelStop(stream);
+	}
+
+	FLYAUDIO_API double getDuration() {
+		unsigned long long len = BASS_ChannelGetLength(stream, BASS_POS_BYTE);
+		return BASS_ChannelBytes2Seconds(stream, len);
+	}
+
+	FLYAUDIO_API double getPosition() {
+		unsigned long long len = BASS_ChannelGetPosition(stream, BASS_POS_BYTE);
+		return BASS_ChannelBytes2Seconds(stream, len);
+	}
+
+	FLYAUDIO_API unsigned long long getDurationB() {
+		unsigned long long len = BASS_ChannelGetLength(stream, BASS_POS_BYTE);
+		return len;
+	}
+
+	FLYAUDIO_API unsigned long long getPositionB() {
+		unsigned long long len = BASS_ChannelGetPosition(stream, BASS_POS_BYTE);
+		return len;
+	}
+
+	FLYAUDIO_API void setPosition(double sec) {
+		unsigned long long pos = BASS_ChannelSeconds2Bytes(stream, sec);
+		BASS_ChannelSetPosition(stream, pos, BASS_POS_BYTE);
+	}
+
+	FLYAUDIO_API void setPositionB(unsigned long long pos) {
+		BASS_ChannelSetPosition(stream, pos, BASS_POS_BYTE);
+	}
+
+	FLYAUDIO_API int setVolume(int vol) {
+		float _vol = (float) (vol < 0 ? 0 : (vol > 100 ? 100 : vol)) / (float) 100.0;
+		BASS_ChannelSetAttribute(stream, BASS_ATTRIB_VOL, _vol);
+		return getVolume();
+	}
+
+	FLYAUDIO_API int getVolume() {
+		float vol;
+		BASS_ChannelGetAttribute(stream, BASS_ATTRIB_VOL, &vol);
+		return (int) (100.0 * vol);
+	}
+
+	FLYAUDIO_API float getCpu() {
+		return BASS_GetCPU();
+	}
+
+	FLYAUDIO_API void freeStream() {
+		BASS_StreamFree(stream);
+		stream = 0;
+	}
+
+	FLYAUDIO_API void close() {
+		BASS_StreamFree(stream);
+		BASS_Free();
+	}
+
+	FLYAUDIO_API AUDIO_META* audioMeta(const char* file) {
+		TagLib::FileRef f(file);
+		AUDIO_META* meta = new AUDIO_META();
+		TagLib::Tag* tag = f.tag();
+		meta->bitRate = f.audioProperties()->bitrate();
+		meta->channels = f.audioProperties()->channels();
+		meta->length = f.audioProperties()->length();
+		meta->sampleRate = f.audioProperties()->sampleRate();
+		
+		meta->title = tag->title().toCString(true);
+		meta->artist = tag->artist().toCString(true);
+		meta->album = tag->album().toCString(true);
+		meta->comment = tag->comment().toCString(true);
+		meta->genre = tag->genre().toCString(true);
+		meta->year = tag->year();
+		meta->track = tag->track();
+
+		const char** otherProps = 
+			new const char* []{"ALBUMARTIST", "SUBTITLE", "DISCNUMBER", "DATE", "ORIGINALDATE", "COMPOSER", "LYRICIST", "CONDUCTOR", "REMIXER", "PERFORMER"};
+		TagLib::PropertyMap map = tag->properties();
+		
+		
+	
+		free(tag);
+		return meta;
+	}
+}
---
+++ FlyAudio.h
@@ -0,0 +1,91 @@
+﻿// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 FLYAUDIO_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// FLYAUDIO_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+
+#ifdef _WIN32
+
+#ifdef FLYAUDIO_EXPORTS
+#define FLYAUDIO_API __declspec(dllexport)
+#else
+#define FLYAUDIO_API __declspec(dllimport)
+#endif
+
+#else
+
+#define FLYAUDIO_API
+
+#endif // _WIN32
+
+
+struct DEVICE_INFO {
+#if defined(_WIN32_WCE) || (WINAPI_FAMILY && WINAPI_FAMILY!=WINAPI_FAMILY_DESKTOP_APP)
+	const wchar_t* name;	// description
+	const wchar_t* driver;	// driver
+#else
+	const char* name;	// description
+	const char* driver;	// driver
+#endif
+	unsigned long long flags;
+	bool isDefault;
+};
+
+/* 记录音频文件信息，来自ID3(v2)标签 */
+struct AUDIO_META {
+	// basic info
+	int length; // s
+	int bitRate; // kb/s
+	int channels;
+	int sampleRate; // Hz
+
+	// id3 tags
+	const char* title;
+	const char* artist;
+	const char* album;
+	const char* comment;
+	const char* genre;
+	int year;
+	int track;
+
+	// other id3 tags
+	const char* albumartist;
+	const char* subtitle;
+	int discnumber;
+	int date;
+	int originaldate;
+};
+
+extern "C" {
+	/* Player control */
+	FLYAUDIO_API void init(int, int);
+	FLYAUDIO_API DEVICE_INFO** getDevices();
+	FLYAUDIO_API int getDeviceCount();
+	FLYAUDIO_API bool setDevice(int);
+	FLYAUDIO_API int loadFile(const char*);
+
+	FLYAUDIO_API bool play();
+	FLYAUDIO_API bool pause();
+	FLYAUDIO_API bool stop();
+
+	FLYAUDIO_API double getDuration();
+
+	FLYAUDIO_API double getPosition();
+	FLYAUDIO_API void setPosition(double);
+
+	FLYAUDIO_API unsigned long long getDurationB();
+	FLYAUDIO_API unsigned long long getPositionB();
+	FLYAUDIO_API void setPositionB(unsigned long long);
+
+	FLYAUDIO_API int setVolume(int);
+	FLYAUDIO_API int getVolume();
+
+	FLYAUDIO_API float getCpu();
+
+	FLYAUDIO_API void freeStream();
+	FLYAUDIO_API void close();
+
+	/* Audio meta */
+	FLYAUDIO_API AUDIO_META* audioMeta(const char* file);
+}
